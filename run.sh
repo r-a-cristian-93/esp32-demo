ab400068af26e175a33cc8fdda80735918e71f3b
alex <r.a.cristian.93@gmail.com>
2023-06-01 23:49:19 +0300

    added UART communication in test-timers demo

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 
 export ESP_FILE=$1
 
-qemu-system-xtensa -nographic -M esp32 -m 4 -drive file=$ESP_FILE,if=mtd,format=raw -gdb tcp::1234,nowait -serial pty -S
+qemu-system-xtensa -nographic -M esp32 -m 4 -drive file=$ESP_FILE,if=mtd,format=raw -gdb tcp::1234,nowait -serial pty -serial pty -S
+
+
+
--- test-timers/main/main.c
+++ test-timers/main/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/timers.h"
 #include "freertos/semphr.h"
 #include "esp_log.h"
+#include "driver/uart.h"
+
+QueueHandle_t uart_queue;
 
 enum {
     TIMER_BEACON,
@@ -17,7 +21,9 @@ int pots;
 SemaphoreHandle_t pot_mutex;
 
 void beaconSignal(TimerHandle_t timer) {
+    char* msg = "MACHINE IS ALIVE";
     ESP_LOGI("BEACON", "MACHINE IS ALIVE");
+    uart_write_bytes(UART_NUM_1, (const char*) msg, strlen(msg));
 }
 
 void potFurnace(TimerHandle_t timer) {
@@ -36,6 +42,21 @@ void productionReport(TimerHandle_t timer) {
 
 void app_main(void)
 {
+    // settup serial communication
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
+        .rx_flow_ctrl_thresh = 122,
+    };
+    const int uart_buffer_size = (1024 * 2);
+
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM_1, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_1, 4, 5, 18, 19));
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_1, uart_buffer_size, uart_buffer_size, 10, &uart_queue, 0));
+
     pot_mutex = xSemaphoreCreateMutex();
 
     timers[TIMER_BEACON] = xTimerCreate(
